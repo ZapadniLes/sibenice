254e1fcc4f0f917be5667b3739c98dac00406d1e
matybarta <matybarta@email.cz>
2023-03-30 10:11:57 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import random
 
-list_pis = []
   
 with open("./slova.txt", "r", encoding="utf8") as file:
     slova = file.read().splitlines()
 
 slovo = print(random.choice(slova))
 
-
-
-pismeno = input("Jaké písmeno hádáš: ")
