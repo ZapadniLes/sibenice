a205d5c3cd8253d3e419c2e5b97bb10f6231d939
ZapadniLes <penc.vojta@seznam.cz>
2023-03-30 10:09:36 +0200

    modified:   main.py 	modified:   sibenice.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#cc
+import random
+
+list_pis = []
+  
+with open("./slova.txt", "r", encoding="utf8") as file:
+    slova = file.read().splitlines()
+
+slovo = print(random.choice(slova))
+
+
+
+pismeno = input("Jaké písmeno hádáš: ")
--- sibenice.py
+++ sibenice.py
@@ -1,12 +1,1 @@
-import random
-
-list_pis = []
-  
-with open("./slova.txt", "r", encoding="utf8") as file:
-    slova = file.read().splitlines()
-
-slovo = print(random.choice(slova))
-
-
-
-pismeno = input("Jaké písmeno hádáš: ")
+#Tady bude generování oběšenýho týpka :)
