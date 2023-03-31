b18b75c91fe8d3b5ade3bfd7d0972d2ce1374642
ZapadniLes <penc.vojta@seznam.cz>
2023-03-31 16:05:42 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ chyba=0
 scan=0
 
 while(chyba!= delka):
+    scan=0
     poprava=sibenice.obesenec[chyba]
     pismeno = input("Jaké písmeno hádáš: ")
     for i in hadanka:
@@ -22,8 +23,8 @@ while(chyba!= delka):
         if (i != pismeno):
            scan=scan+1
 
-           if (scan>delka):
-               chyba=chyba+1
+    if (scan>0):
+        chyba=chyba+1
     print(chyba)
     print(poprava)
             
