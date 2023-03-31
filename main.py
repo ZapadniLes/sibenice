d6112967bbcbbcae046f30df7bcfa27e78c36216
matybarta <matybarta@email.cz>
2023-03-31 22:20:48 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import random
 import sibenice
+import os
 with open("./slova.txt", "r", encoding="utf8") as file:
     slova = file.read().splitlines()
 
 slovos = str(random.choice(slova))
 
 delka= len(slovos)
-print(delka)
+print("Slovo má tolik písmen: ", delka)
 chyba=0
 odpovedi=""
 
@@ -14,12 +15,13 @@ odpovedi=""
 print(slovos)
 #print(hadanka)
 
-while(chyba != 7):
+while(chyba != 8):
     scan=0
     poprava=sibenice.obesenec[chyba]
     pismeno = input("Jaké písmeno hádáš: ")
     for i in slovos:
-            
+
+        odpovedi=odpovedi+pismeno        
         if i in odpovedi:
            #scan= scan+1
            print(i, end="")
@@ -27,22 +29,16 @@ while(chyba != 7):
             print("_", end="")
             scan = scan+1
 
-    if scan ==0:
+    if scan == 0:
+        print("\n", "Gratuluji uhádl jsi dané slovo (" + slovos + ") a vyhrál jsi.")
         break
 
-    odpovedi=odpovedi+pismeno
 
     if pismeno not in slovos:
         chyba=chyba+1
 
 
 
-    """if (scan>0):
-        chyba=chyba+1"""
-
-
 
     print("\n", "počet chyb: ", chyba)
     print(poprava)
-
-
